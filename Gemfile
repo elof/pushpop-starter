3416bc5fce58afd4c64ea48241982e6a9e350896
Justin Johnson <elofjohnson@gmail.com>
2015-12-08 14:26:05 -0800

    first commit

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source 'https://rubygems.org'
 
 gem 'pushpop'
+gem 'keen'
+gem 'gems'
 
 # uncomment plugins if needed
 # gem 'pushpop-keen'
--- jobs/example_job.rb
+++ jobs/example_job.rb
@@ -1,25 +1,28 @@
 require 'pushpop'
+require 'keen'
+require 'gems'
 
-# This create the very basic setup of the job - essentially just gives it a name
-job 'Simple Math' do
 
-  # This will tell the job to run every 5 seconds
-  every 5.seconds
+job 'Ruby SDK' do
 
-  # Very simply, the first step of this job returns `10` every time it runs
-  step 'return 10' do 10 end
+    every 24.hours
 
-  # This will increment whatever the first step returned by 20, and respond with that
-  step 'increase by 20' do |response|
-    20 + response
-  end
+    step 'webhook' do
+      Gems.info 'keen'
+      # Gems.add_web_hook 'keen', 'https://api.keen.io/3.0/projects/5011efa95f546f2ce2000000/events/ruby_sdk?api_key=d71fcdb488a6d064ba6308a9242b9f263e981f521f03d16942a407c802b2bdade581ac093f815975b9abcc73788e8e48a01b7382c804d1df4fb582bfac6468ee507b0e75fd3fcbd8c7db8ba3074f383e4721a3d7916d217e4a7e4f85fda3ee8e5fae56fc3b8a4bf17eb2f7c41213ee6e'
+    end
 
-  # This outputs a very basic HTML template containing the results of the math in the previous steps
-  step 'print out via template' do |response|
-    html = template 'first_template.html.erb', response
-    puts 'Hey Pushpop, let\'s do a math!'
-    puts html
-    html
-  end
+    # This outputs a very basic HTML template containing the results of the math in the previous steps
+    # step 'print out via template' do |response|
+    #   html = template 'first_template.html.erb', response
+    #   puts 'works'
+    #   puts html
+    #   html
+    # end
+
+
+    step 'push to Keen IO' do |response|
+        Keen.publish(:ruby_sdk, response )
+    end
 
 end
